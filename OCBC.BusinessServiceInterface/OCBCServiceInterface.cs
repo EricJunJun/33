cbe2368f30df32e215486858f97925bcf14ef92e
EricXu <1002645394@qq.com>
2021-07-09 10:01:57 +0800

    Initial commit

---
+++ OCBC.BusinessServiceInterface/OCBCServiceInterface.cs
@@ -0,0 +1,14 @@
+﻿using OCBC.BusinessEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCBC.BusinessServiceInterface
+{
+    public interface OCBCServiceInterface
+    {
+        Guid SaveRegisterDetails(User user);
+    }
+}
